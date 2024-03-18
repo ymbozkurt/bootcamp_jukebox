1e0258c06facc1535ab7ece5f6da05d1ea596fbe
Yavuz Mert Bozkurt <97530216+ymbozkurt@users.noreply.github.com>
2024-03-18 03:51:07 +0300

    End of program coding

--- programs/bootcamp_jukebox/src/lib.rs
+++ programs/bootcamp_jukebox/src/lib.rs
@@ -1,14 +1,20 @@
 use anchor_lang::prelude::*;
-use std::time::Duration;
 
-declare_id!("GCYSF7p2FQpHNDejqYXanimkvdxD67efGwFJ2Sp1S6wp");
+declare_id!("A3wMAxHeZ1rRgHT6NGRATXUtV69KVaN5YtR5gZwweA44");
 
 #[program]
 mod jukebox {
     use super::*;
+
+    pub fn create_jukebox(ctx: Context<CreateJukebox>) -> Result<()> {
+        let jukebox = &mut ctx.accounts.jukebox;
+        jukebox.initialize();
+        jukebox.owner = *ctx.accounts.authority.key;
+        Ok(())
+    }
+
     pub fn add_to_queue(ctx: Context<AddToQueue>, song_no: u8) -> Result<()> {
         let jukebox = &mut ctx.accounts.jukebox;
-        let duration = jukebox.song_duration[(song_no-1) as usize];
         let song_name: String = (&jukebox.song_list[(song_no-1) as usize]).to_string();
         jukebox.playing_next.push(song_name);
 
@@ -20,6 +26,7 @@ mod jukebox {
         (&jukebox.playing_next).to_vec();
         Ok(())
     }
+
 
 }
 
@@ -37,18 +44,26 @@ pub struct GetList<'info> {
     jukebox: Account<'info, Jukebox>,
 }
 
+#[derive(Accounts)]
+pub struct CreateJukebox<'info> {
+    #[account(init, payer = authority, space = 8 + 40 + 10 * 32 + 10 + 1000)]
+    jukebox: Account<'info, Jukebox>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+
 
 #[account]
 pub struct Jukebox {
     owner: Pubkey,
     song_list: [String; 10], // JukeBox'ta bulunan tüm şarkıların listesi
     playing_next: Vec<String>,   // Sıradaki şarkı listesi
-    song_duration: [u8; 10],
     }
 
 impl<'info> Jukebox {
     pub fn initialize(&mut self) {
-        self.owner = Pubkey::from_str("Mert7acJinRgH3BYtT3QVyYApf46AefSzXv2Lpt5gdF");
         self.song_list = [
             "The Weekend - Moth To A Flame".to_string(),
             "Özkan Uğur - Bir Bakman Lazım".to_string(),
@@ -61,17 +76,5 @@ impl<'info> Jukebox {
             "Ezhel - Allah'ından Bul".to_string(),
             "Led Zeppelin - Baby I'm Gonna Leave You".to_string(),
         ];
-        self.song_duration = [
-            180,
-            200,
-            230,
-            170,
-            185,
-            180,
-            200,
-            230,
-            170,
-            185,
-        ]
-    }
+    }
 }
